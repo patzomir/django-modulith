1aa25cb82cea18a69a6341ef8af276094ac84324
Plamen Tarkalanov <plamen.tarkalanov@dataops.live>
2025-03-18 16:15:59 +0200

    feat: Improve stub generation and test setup
    This commit enhances the stub generation process and improves the test setup.
    
    -   Updates the `.vscode/settings.json` to trigger stub generation when `modulith.py` files are saved. This change replaces the previous approach with a more targeted and efficient method. The command now uses `manage.py` to run the `generatestubs` command, ensuring the correct environment is used.
    -   Adds a second sample interface function `sample_function2` in `tests/test_modulith/modulith.py` to increase test coverage.
    -   Updates `tests/manage.py` to use `django_modulith_test.settings` to ensure the tests run correctly.

--- tests/test_modulith/modulith.py
+++ tests/test_modulith/modulith.py
@@ -4,3 +4,8 @@ from django_modulith.interface_decorator import interface
 @interface
 def sample_function(cls, param: str) -> str:
     return f"Called with {param}"
+
+
+@interface
+def sample_function2(cls, param: str) -> str:
+    return f"Called with {param}"
